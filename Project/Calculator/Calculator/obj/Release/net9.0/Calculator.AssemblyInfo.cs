b55edab35d88ef79998117d40449dc94f20e88ea
RocketRahul321 <rahulsarkrbejoy321@gmail.com>
2025-11-06 16:17:02 +0600

    23-51556-2

--- Project/Calculator/Calculator/obj/Release/net9.0/Calculator.AssemblyInfo.cs
+++ Project/Calculator/Calculator/obj/Release/net9.0/Calculator.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9f493a59f7fa25f07ea030e668075d9677a70c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
---
+++ Project/MortgageCalculator/MortgageCalculator/Program.cs
@@ -0,0 +1,37 @@
+﻿
+        using System.Globalization;
+
+          Console.WriteLine("Enter three  values of loan annual ");
+
+        Console.WriteLine("Enter value of loan (p):");
+
+        double loan = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the value of annual interest rate (r):");
+
+        double annualInterest = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the number of payments you have paid (n):");
+
+        int numberOfPayments = Convert.ToInt32(Console.ReadLine());
+
+       
+        double monthlyInterest = annualInterest / 12 / 100;
+
+      
+        double monthlyPayment = loan * monthlyInterest / (1 - Math.Pow(1 + monthlyInterest, -numberOfPayments));
+
+        
+        double totalPaid = monthlyPayment * numberOfPayments;
+
+        double totalInterest = totalPaid - loan;
+
+        
+        CultureInfo culture = new CultureInfo("bn-BD");
+
+        Console.WriteLine($"The monthly mortgage payment is: {monthlyPayment.ToString("C", culture)}");
+
+        Console.WriteLine($"The total paid is: {totalPaid.ToString("C", culture)}");
+
+        Console.WriteLine($"The total interest is: {totalInterest.ToString("C", culture)}");
+    
