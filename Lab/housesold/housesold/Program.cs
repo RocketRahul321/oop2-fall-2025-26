7b150fd64b24859378552244f8a7c304aa4a9dc1
RocketRahul321 <rahulsarkrbejoy321@gmail.com>
2025-11-12 15:15:57 +0600

    23-51556-2

---
+++ Lab/housesold/housesold/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class House
+{
+    public int BuildYear { get; set; }
+    public double Size { get; set; }
+
+    public House(int buildYear, double size)
+    {
+        BuildYear = buildYear;
+        Size = size;
+    }
+
+    public int HowOld()
+    {
+        int currentYear = DateTime.Now.Year;
+        int age = currentYear - BuildYear;
+        return age;
+    }
+
+    public void CanBeSold()
+    {
+        int age = HowOld();
+        if (age >= 15)
+        {
+            Console.WriteLine("The house is old enough to be sold.");
+        }
+        else
+        {
+            Console.WriteLine("The house cannot be sold.");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the build year of the house: ");
+
+        int buildYear = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the size of the house: ");
+
+        double size = Convert.ToDouble(Console.ReadLine());
+
+        House myHouse = new House(buildYear, size);
+
+        
+        Console.WriteLine($"The age of the house is about {myHouse.HowOld()} years old.");
+
+        myHouse.CanBeSold();
+    }
+}
