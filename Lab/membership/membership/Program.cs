3e454a41451b90a9e32b2549fa28aafc26f771e1
RocketRahul321 <rahulsarkrbejoy321@gmail.com>
2025-12-10 12:16:29 +0600

    23-51556-2

---
+++ Lab/membership/membership/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+public abstract class Member
+{
+    protected string Name;
+    protected int memberId;
+
+    public Member(string name, int memberId)
+    {
+        this.Name = name;
+        this.memberId = memberId;
+    }
+
+    public abstract double CalculateFees();
+}
+public class Student : Member
+{
+    public Student(string name, int memberId) : base(name, memberId) { }
+
+    public override double CalculateFees()
+    {
+        return 300;
+    }
+}
+
+public class Ragular : Member
+{
+    public Ragular(string name, int memberId) : base(name, memberId) { }
+
+    public override double CalculateFees()
+    {
+        return 500;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Member s = new Student("Rahul", 51556);
+        Member r = new Ragular("Amin", 52578);
+
+        Console.WriteLine($"Student fees: {s.CalculateFees()}");
+        Console.WriteLine($"Ragular fees: {r.CalculateFees()}");
+    }
+}
