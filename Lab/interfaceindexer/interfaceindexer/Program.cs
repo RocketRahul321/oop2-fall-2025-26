29a862b7bf9cad4348c68a02c6f58a1454abd17a
RocketRahul321 <rahulsarkrbejoy321@gmail.com>
2025-12-15 14:08:03 +0600

    23-51556-2

---
+++ Lab/interfaceindexer/interfaceindexer/Program.cs
@@ -0,0 +1,69 @@
+﻿//question no.1
+
+using System;
+
+interface Irpintable
+{
+    void Print();
+}
+
+interface Iserializable
+{
+    void save();
+}
+
+//interface Istudent
+//{
+//    void display();
+//}
+
+class Rrport :Irpintable, Iserializable
+{
+    public void Print()
+    {
+        Console.WriteLine("Printing Report");
+    }
+    public void save()
+    {
+        Console.WriteLine("Saving Report");
+    }
+}
+
+//class student:Istudent
+//{
+//    private int[] marks= new int[3];
+
+//    public int this[int index]
+//    {
+//        get { return marks[index]; }
+//        set { marks[index] = value; }
+//    }
+//    public void display()
+//    {
+//        Console.WriteLine("Displaying Student Information");
+//        for (int i = 0; i < marks.Length; i++)
+//        {
+//            Console.WriteLine("Score " + (i + 1) + ": " + marks[i]);
+//        }
+//    }
+//}
+
+class Program
+{
+  static void Main()
+  {
+        Rrport rpt = new Rrport();
+       // student stu = new student();
+
+       //stu[0] = 88;
+       //stu[1] = 92;
+       //stu[2] = 85;
+
+       //stu.display();
+
+         rpt.Print();
+            rpt.save();
+    }
+}
+
+
---
+++ Lab/interfaceindexerq2/interfaceindexerq2/Program.cs
@@ -0,0 +1,49 @@
+﻿
+//question no.2
+
+using System;
+
+
+
+interface Istudent
+{
+    void display();
+}
+
+
+
+class student : Istudent
+{
+    private int[] marks = new int[3];
+
+    public int this[int index]
+    {
+        get { return marks[index]; }
+        set { marks[index] = value; }
+    }
+    public void display()
+    {
+        Console.WriteLine("Displaying Student Information");
+        for (int i = 0; i < marks.Length; i++)
+        {
+            Console.WriteLine("Score " + (i + 1) + ": " + marks[i]);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+
+        student stu = new student();
+
+        stu[0] = 99;
+        stu[1] = 60;
+        stu[2] = 88;
+
+        stu.display();
+
+
+    }
+}
